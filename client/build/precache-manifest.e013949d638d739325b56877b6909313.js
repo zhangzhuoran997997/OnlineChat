c50ffa8fdcbb3eb34b7bd52fb5b4c4427ba7965c
zhangzhuoran997997 <63936820+zhangzhuoran997997@users.noreply.github.com>
2022-06-08 12:10:04 +0800

    frontend

---
+++ client/build/precache-manifest.e013949d638d739325b56877b6909313.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8a568e3cde386a3b68e2a0afd8f785",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38222a034de22621c13c",
+    "url": "/static/js/2.75fb539d.chunk.js"
+  },
+  {
+    "revision": "b510064844bec3e9e4c1",
+    "url": "/static/js/main.cee3ff05.chunk.js"
+  },
+  {
+    "revision": "fd64685ce737b4bc5f61",
+    "url": "/static/js/runtime~main.c1c21129.js"
+  }
+]);
